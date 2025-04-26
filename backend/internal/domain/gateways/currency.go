b599fa64ced0f96ee9e5611c5137452571eb017f
Otavio Barbosa Alves Martins <taviomartins01@gmail.com>
2025-04-26 20:01:23 -0300

    refactor: adicionar interface CurrencyMetadataGateway

--- backend/internal/domain/gateways/currency.go
+++ backend/internal/domain/gateways/currency.go
@@ -13,6 +13,9 @@ type CurrencyConversion struct {
 }
 
 type CurrencyGateway interface {
-	// Converte um valor de USD para a moeda destino com base na data (ou datas anteriores até 6 meses)
 	ConvertUSDTo(currency string, date time.Time, amount float64) (*CurrencyConversion, error)
+}
+
+type CurrencyMetadataGateway interface {
+	GetAvailableCurrencies() ([]string, error)
 }
